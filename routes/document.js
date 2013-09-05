7e9a19966c3814c2260105b811b73b6f2871dfc8
Daniel Ayala <dmayala@csupomona.edu>
2013-09-05 09:58:33 -0700

    Finish Tutorial
    Client Javascript rewritten in Backbone.js

--- routes/document.js
+++ routes/document.js
@@ -11,7 +11,13 @@ exports.listDoc = function(req, res) {
         break;
 
       default:
-        res.render('documents/index.jade', { documents: documents, currentUser: req.currentUser });
+      documents = documents.map(function(d) {
+        var clone = d.toJSON();
+        clone.id = d._id;
+        return clone;
+      });
+
+      res.render('documents/index.jade', { documents: documents, currentUser: req.currentUser });
     }
   });
 };
@@ -19,7 +25,7 @@ exports.listDoc = function(req, res) {
 exports.listTitle = function (req, res) {
   Document.find({user_id: req.currentUser.id}).sort({title: -1}).exec(function (err, documents) {
     res.send(documents.map(function(d) {
-      return {title: d.title, _id: d._id};
+      return { title: d.title, id: d._id };
     }));
   });
 }
@@ -36,12 +42,14 @@ exports.newDoc = function(req, res) {
 };
 
 exports.createDoc = function(req, res) {
-  var d = new Document(req.body.document);
+  var d = new Document(req.body);
   d.user_id = req.currentUser.id;
   d.save(function() {
     switch (req.params.format) {
       case 'json':
-        res.send(d.toObject());
+        var data = d.toJSON();
+        data.id = data._id;
+        res.send(data);
       break;
 
       default:
@@ -73,8 +81,8 @@ exports.updateDoc = function(req, res, next) {
   Document.findOne({ _id: req.params.id, user_id: req.currentUser.id }, function(err, d) {
     if (!d) { return next(new Error('Document not found')); } 
     
-    d.title = req.body.d.title;
-    d.data = req.body.d.data;
+    d.title = req.body.title;
+    d.data = req.body.data;
 
     d.save(function(err) {
       switch (req.params.format) {
@@ -114,8 +122,12 @@ exports.search = function(req, res) {
             switch (req.params.format) {
               case 'json':
                 res.send(documents.map(function (d) {
-                  return {title: d.title, _id: d._id};
+                  return { title: d.title, id: d._id };
                 }));
+              break;
+
+              default:
+                res.send('Format not available', 406);
               break;
             }
           });
